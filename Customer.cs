9718f7e5d418db2f17dbbfa692b78a9b8dafadbe
Mike Wallace <mryanwallace@gmail.com>
2018-09-05 17:33:54 -0700

    Modified Customer so that it implements OnPropertyChanged(), necessary for or oror two-way binding

--- Customer.cs
+++ Customer.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UWPTestApp
 { 
-    public class Customer
+    public class Customer : INotifyPropertyChanged
     {
-        public string Name { get; set; }
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            // Raises the OnPropertyChanged event to enable two-way data binding
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
