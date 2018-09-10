e32a45e8f923a735ec039d628d30dd60e32792e2
Mike Wallace <mryanwallace@gmail.com>
2018-09-09 19:43:40 -0700

    Added RSSFeedReader class that just uses the Syndication API to download an RSS feed and store it in a private string

---
+++ RSSFeedReader.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Web.Syndication;
+
+namespace UWPTestApp
+{
+    class RSSFeedReader
+    {
+        private string _rssResult = "Null";
+
+        public string RSSResult
+        {
+            get { return _rssResult; }
+            set { _rssResult = value; }
+        }
+        public async Task downloadRSSFeedAsync(string url, ObservableCollection<Customer> Customers)
+        {
+            Windows.Web.Syndication.SyndicationClient client = new SyndicationClient();
+
+            Uri feedUri
+                = new Uri(url);
+
+            try
+            {
+                SyndicationFeed feed = await client.RetrieveFeedAsync(feedUri);
+
+                // The rest of this method executes after await RetrieveFeedAsync completes.
+                RSSResult = feed.Title.Text + Environment.NewLine;
+
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    RSSResult += item.Title.Text + ", " +
+                                     item.PublishedDate.ToString() + Environment.NewLine;
+                }
+                Customers.Add(new Customer() { Name = RSSResult });
+            }
+            catch (Exception ex)
+            {
+                // Log Error.
+                RSSResult =
+                    "I'm sorry, but I couldn't load the page," +
+                    " possibly due to network problems." +
+                    "Here's the error message I received: "
+                    + ex.ToString();
+            }
+        }
+    }
+}
