f91b8e404cd6fb4198965fe2bfb0f3b9c7263c1e
Mike Wallace <mryanwallace@gmail.com>
2018-09-09 19:42:14 -0700

    Added timer to main page that refreshes RSS feed every 5 seconds

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -23,17 +24,45 @@ namespace UWPTestApp
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        DispatcherTimer dispatcherTimer;
+        RSSFeedReader rss_0;
+        RSSFeedReader rss_1;
+        RSSFeedReader rss_2;
+
         public ObservableCollection<Customer> Customers { get; }
             = new ObservableCollection<Customer>();
-
 
         public MainPage()
         {
             this.InitializeComponent();
+            rss_0 = new RSSFeedReader();
+            rss_1 = new RSSFeedReader();
+            rss_2 = new RSSFeedReader();
+            DispatcherTimerSetup();
+        }
 
-            this.Customers.Add(new Customer() { Name = "NAME1" });
-            this.Customers.Add(new Customer() { Name = "NAME2" });
-            this.Customers.Add(new Customer() { Name = "NAME3" });
+
+        public void DispatcherTimerSetup()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            // 30 Second Timer
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 30);
+            dispatcherTimer.Start();
+        }
+
+        void dispatcherTimer_Tick(object sender, object e)
+        {
+            // Read RSS Feeds every tick of the timer
+            rss_0.downloadRSSFeedAsync("https://news.xbox.com/en-us/feed/stories/", Customers);
+            rss_1.downloadRSSFeedAsync("http://feeds.reuters.com/reuters/scienceNews", Customers);
+            rss_2.downloadRSSFeedAsync("https://www.npr.org/rss/podcast.php?id=510308", Customers);
+
+        }
+
+        private void Page_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            DispatcherTimerSetup();
         }
     }
 }
